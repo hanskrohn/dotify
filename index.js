76ae7c786ef0ca9f29ad180c1c2eaeb8b5644f6c
Hans Krohn <hanskrohn70@gmail.com>
2019-07-08 12:32:29 -0500

    testing with api

--- index.js
+++ index.js
@@ -4,5 +4,18 @@
         return response.json()
     })
     .then(function(result){
-        console.log(results)
+        console.log(result)
     })
+id = '3n3Ppam7vgaVa1iaRUc9Lp'    
+
+token = 'BQA3KJN29pOxnnZTNBl045MacCJHazyaK64qrpbk0rqfOvnYCrSJraTZ-88wWBz-TuQO_GCPvdiBR6db9riP4pMbNErP5WNG-qxqFJuv02uE-6QZKX1sISC8GP6Xhz-rvsI5FfpRRC9UdSvdLR1inU5ZAwFW7vy4YSlbenaHXI_2MadT6cnhX8NRMjSpfp1bvZWsSbARRZ7kuTUNWbWAvS9EeVU98kmU6gXwv0JUMKxVHxsPaJ-r-bf-vwJD0mPGpY6FGaj4jg3A656ObuBTc4OTUncvEMWB3o4'
+
+fetch("https://api.spotify.com/v1/tracks/" + id, {
+  method: "GET",
+  headers: {
+    Authorization: "Bearer " + token     
+  }
+})
+.then(response => response.json())
+.then((beats) => { console.log(beats)
+})
