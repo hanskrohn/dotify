f412007e2ae9b3136c640758d2dc98412af96634
Hans Krohn <hanskrohn70@gmail.com>
2019-07-10 11:04:19 -0500

    cleaned up where reshresh is sent

--- server/app/controllers/songs_controller.rb
+++ server/app/controllers/songs_controller.rb
@@ -1,36 +1,17 @@
 class SongsController < ApplicationController
     def index
         # @artists = RSpotify::Artist.search(input)
-        
+
         
     end
     def show
+
         input = params[:input]
         # @artists = RSpotify::Artist.search(input)
         # song = Song.find(params[:id])
         render json: @artist
     end
-    def refresh
-        begin
-            #Rest client search. This asks spotify to give us an access_token and a refresh_token
-            #This where the param requirements they asked for (available in there documentation)
-            response = RestClient.post 'https://accounts.spotify.com/api/token', {redirect_uri: 'http://localhost:3000/callback',
-                    grant_type: 'authorization_code',
-                    #params[:code] was the long url sent from the if(!_token)
-                    code: params[:code],
-                    client_id: '55c12b17da23491c82c85edd1c942d61',
-                    client_secret: '190d444411b04417816ac4550e9115c4'}, {'Content-Type': 'application/x-www-form-urlencoded'}
-            rescue Exception => e 
-                byebug
-            end
-        #They return a stringified version of the code
-        #we needed to parse over the info and make it appear like an object ({})
-        result = JSON.parse(response.body)
-        access_token =result["access_token"]
-        refresh_token = result["refresh_token"]
-        #From here we reply only using the access_token as the refresh_token is not required nir ever used in the front end
-        redirect_to "http://10.185.4.208:8080/#access_token=#{access_token}" #&refresh_token=#{refresh_token}"
-    end
+  
 
 
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs
   get '/callback', to: 'songs#refresh'
-  get '/controller/show/:input', to: 'songs#show'
+  get '/controller/show/:input', to: 'application#show'
 end
